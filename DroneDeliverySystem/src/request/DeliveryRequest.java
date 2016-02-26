12659f7ec2a5a93941cfc81320c65d9e0ffdf8b8
Partsaleva <partsaleva@gmail.com>
2016-02-26 13:21:02 +0200

    Added class Product, update City and Warehouse

---
+++ DroneDeliverySystem/src/request/DeliveryRequest.java
@@ -0,0 +1,19 @@
+package request;
+
+import java.util.Date;
+
+public class DeliveryRequest implements Request{
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getTimestamp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
